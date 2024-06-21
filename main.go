de218fde76113261146dbd80502a4e87c8ec464b
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2024-06-20 21:37:39 -0400

    feat: standup testing

--- main.go
+++ main.go
@@ -5,11 +5,16 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
+	guildId, exists := os.LookupEnv("GUILD_ID")
+	if !exists {
+		log.Fatalln("GUILD_ID missing from environment")
+	}
 	botToken, exists := os.LookupEnv("BOT_AUTH_TOKEN")
 	if !exists {
 		log.Fatalln("BOT_AUTH_TOKEN missing from environment")
@@ -19,14 +24,24 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	discord.AddHandler(messageCreate)
+	discord.AddHandler(handleStandup)
+
+	standupCommand := discordgo.ApplicationCommand{
+		Name:        "standup",
+		Description: "what are you upto today!?",
+	}
+	ccmd, err := discord.ApplicationCommandCreate(discord.State.User.ID, guildId, &standupCommand)
+
+	if err != nil {
+		log.Panicln(err)
+	}
 
 	discord.Identify.Intents = discordgo.IntentGuildMessages
 
 	err = discord.Open()
 
 	if err != nil {
-		log.Fatalln(err)
+		log.Panicln(err)
 	}
 	log.Println("opening connection...")
 
@@ -34,28 +49,44 @@ func main() {
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
 
+	log.Println("Removing commands...")
+
+	if err := discord.ApplicationCommandDelete(discord.State.User.ID, guildId, standupCommand.ID); err != nil {
+		log.Panicf("Cannot delete '%v' command: %v", ccmd.Name, err)
+	}
+
 	discord.Close()
 }
 
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
-	// Ignore all messages created by the bot itself
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-	// If the message is "ping" reply with "Pong!"
-	if m.Content == "ping" {
-		_, err := s.ChannelMessageSend(m.ChannelID, "Pong!")
-		if err != nil {
-			log.Fatalln(err)
+func handleStandup(discord *discordgo.Session, i *discordgo.InteractionCreate) {
+	if i.ApplicationCommandData().Name == "standup" {
+		embed := discordgo.MessageEmbed{
+			URL:         "https://goombi.com",
+			Type:        "rich",
+			Title:       "hello goombi",
+			Description: "goombster",
+			Timestamp:   time.Now().String(),
+			Color:       0,
+			Footer:      &discordgo.MessageEmbedFooter{},
+			Image:       &discordgo.MessageEmbedImage{},
+			Thumbnail:   &discordgo.MessageEmbedThumbnail{},
+			Video:       &discordgo.MessageEmbedVideo{},
+			Author: &discordgo.MessageEmbedAuthor{
+				URL:     "https://amir.day",
+				Name:    "Amir Azizafshari",
+				IconURL: "https://picsum.photos/200",
+			},
+			Fields: []*discordgo.MessageEmbedField{
+				{
+					Name:   "goombi field",
+					Value:  "goombi value",
+					Inline: false,
+				},
+			},
 		}
-	}
 
-	// If the message is "pong" reply with "Ping!"
-	if m.Content == "pong" {
-		_, err := s.ChannelMessageSend(m.ChannelID, "Ping!")
-		if err != nil {
-			log.Fatalln(err)
+		if _, err := discord.ChannelMessageSendEmbed("", &embed); err != nil {
+			log.Fatalf(err.Error())
 		}
 	}
 }
