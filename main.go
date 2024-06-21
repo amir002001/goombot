902b4bfe4b28ad70998d9d88925169c5804a43b1
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2024-06-21 01:59:38 -0400

    feat: standup testing

--- main.go
+++ main.go
@@ -5,45 +5,55 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
-	guildId, exists := os.LookupEnv("GUILD_ID")
+	guildID, exists := os.LookupEnv("GUILD_ID")
 	if !exists {
 		log.Fatalln("GUILD_ID missing from environment")
 	}
+
 	botToken, exists := os.LookupEnv("BOT_AUTH_TOKEN")
 	if !exists {
 		log.Fatalln("BOT_AUTH_TOKEN missing from environment")
 	}
+
 	discord, err := discordgo.New("Bot " + botToken)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
+	log.Println("opening connection...")
+
+	if err = discord.Open(); err != nil {
+		log.Panicln(err)
+	}
+
 	discord.AddHandler(handleStandup)
 
 	standupCommand := discordgo.ApplicationCommand{
-		Name:        "standup",
-		Description: "what are you upto today!?",
+		ID:                       "",
+		ApplicationID:            "",
+		GuildID:                  guildID,
+		Version:                  "",
+		Type:                     0,
+		Name:                     "standup",
+		NameLocalizations:        &map[discordgo.Locale]string{},
+		DefaultPermission:        new(bool),
+		DefaultMemberPermissions: new(int64),
+		DMPermission:             new(bool),
+		NSFW:                     new(bool),
+		Description:              "what are you upto today!?",
+		DescriptionLocalizations: &map[discordgo.Locale]string{},
+		Options:                  []*discordgo.ApplicationCommandOption{},
 	}
-	ccmd, err := discord.ApplicationCommandCreate(discord.State.User.ID, guildId, &standupCommand)
 
+	ccmd, err := discord.ApplicationCommandCreate(discord.State.User.ID, guildID, &standupCommand)
 	if err != nil {
 		log.Panicln(err)
 	}
-
-	discord.Identify.Intents = discordgo.IntentGuildMessages
-
-	err = discord.Open()
-
-	if err != nil {
-		log.Panicln(err)
-	}
-	log.Println("opening connection...")
 
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
@@ -51,7 +61,7 @@ func main() {
 
 	log.Println("Removing commands...")
 
-	if err := discord.ApplicationCommandDelete(discord.State.User.ID, guildId, standupCommand.ID); err != nil {
+	if err := discord.ApplicationCommandDelete(discord.State.User.ID, guildID, standupCommand.ID); err != nil {
 		log.Panicf("Cannot delete '%v' command: %v", ccmd.Name, err)
 	}
 
@@ -65,28 +75,41 @@ func handleStandup(discord *discordgo.Session, i *discordgo.InteractionCreate) {
 			Type:        "rich",
 			Title:       "hello goombi",
 			Description: "goombster",
-			Timestamp:   time.Now().String(),
+			Timestamp:   "",
 			Color:       0,
-			Footer:      &discordgo.MessageEmbedFooter{},
-			Image:       &discordgo.MessageEmbedImage{},
-			Thumbnail:   &discordgo.MessageEmbedThumbnail{},
-			Video:       &discordgo.MessageEmbedVideo{},
+			Footer: &discordgo.MessageEmbedFooter{
+				Text:         "",
+				IconURL:      "",
+				ProxyIconURL: "",
+			},
+			Image: &discordgo.MessageEmbedImage{
+				URL:      "",
+				ProxyURL: "",
+				Width:    0,
+				Height:   0,
+			},
+			Thumbnail: &discordgo.MessageEmbedThumbnail{
+				URL:      "",
+				ProxyURL: "",
+				Width:    0,
+				Height:   0,
+			},
+			Video: nil,
+			Provider: &discordgo.MessageEmbedProvider{
+				URL:  "",
+				Name: "",
+			},
 			Author: &discordgo.MessageEmbedAuthor{
-				URL:     "https://amir.day",
-				Name:    "Amir Azizafshari",
-				IconURL: "https://picsum.photos/200",
+				URL:          "https://amir.day",
+				Name:         "Amir Azizafshari",
+				IconURL:      "https://picsum.photos/200",
+				ProxyIconURL: "",
 			},
-			Fields: []*discordgo.MessageEmbedField{
-				{
-					Name:   "goombi field",
-					Value:  "goombi value",
-					Inline: false,
-				},
-			},
+			Fields: []*discordgo.MessageEmbedField{{Name: "goombi field", Value: "goombi value", Inline: false}},
 		}
 
-		if _, err := discord.ChannelMessageSendEmbed("", &embed); err != nil {
-			log.Fatalf(err.Error())
+		if _, err := discord.ChannelMessageSendEmbed("1217489307431075991", &embed); err != nil {
+			panic(err)
 		}
 	}
 }
